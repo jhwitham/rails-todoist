d6ac47c7b3cd59bc7f42693975d6607684a95b40
John Witham <john.h.witham@gmail.com>
2018-08-18 21:57:13 +0000

    added private find item method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order("created_at DESC")
+  end
+  
+  def show
+    
   end
   
   def new
@@ -20,5 +25,8 @@ class ItemsController < ApplicationController
       def item_params
         params.require(:item).permit(:title, :description)
       end
-    
+      
+      def find_item
+        @item = Item.find(params[:id])
+      end
 end
